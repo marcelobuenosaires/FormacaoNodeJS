ae75e1f2edc6275c692592e19a07463930d0d43b
Marcelo Moura <45643146+marcelobuenosaires@users.noreply.github.com>
2020-04-18 16:12:21 -0300

    calculadora feita em node.js
    calculadora feita em node.js com as operações básicas.

---
+++ calculadora/calculadora.js
@@ -0,0 +1,37 @@
+var nome = "Minha calculadora Marcelo V1";
+
+function soma(a,b){
+return a + b;
+
+
+
+return (a + b);
+}
+
+function mult(a,b){
+    return (a * b);
+
+}
+
+function sub(a,b){
+    return (a-b);
+
+}
+
+function div(a,b){
+    return (a/b);
+
+}
+
+module.exports = {
+
+    soma,
+    mult,
+    sub,
+    div, 
+    nome
+
+
+
+
+};
